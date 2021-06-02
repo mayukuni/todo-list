7408e1ebfa7830522e8a9906c3e3c23a42170b55
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-06-02 19:44:13 -0300

    adiciona requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const button = document.getElementById('criar-tarefa');
 let list = document.getElementById('lista-tarefas'); //ol
 
+//5 e 6
+const button = document.getElementById('criar-tarefa');
 function addTask() {
   //pegar o conteudo do input e adicionar como uma nova li da ol
   let inputValue = document.querySelector('input').value; //input
@@ -11,12 +12,33 @@ function addTask() {
 }
 button.addEventListener('click', addTask);
 
+
+//7 
 function changeColor(event) {
   let grayColor = 'rgb(128,128,128)'
   let clickedLi = event.target
   clickedLi.style.backgroundColor = grayColor
 }
 list.addEventListener('click', changeColor);
+
+// 8
+let li = document.querySelectorAll('li')
+
+// function removeBackgroundColor() {
+  //   for (let index = 0; index < li.length; index += 1) {
+    //     if (li[index].style.backgroundColor === 'rgb(128,128,128)') {
+      //       li[index].style.backgroundColor = 'white';
+      //     }
+      //   }
+      // }
+      // li.addEventListener('click', removeBackgroundColor)
+      
+// 10
+const clearButton = document.getElementById('apaga-tudo');
+function clearAll() {
+  list.innerHTML = ''
+}
+clearButton.addEventListener('click', clearAll)
 
 
 

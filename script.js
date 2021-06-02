e9601fc7634e1a61ff14d843d906f71761c5900d
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-06-02 18:30:32 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const button = document.getElementById('criar-tarefa');
-
+let list = document.getElementById('lista-tarefas'); //ol
 
 function addTask() {
   //pegar o conteudo do input e adicionar como uma nova li da ol
   let inputValue = document.querySelector('input').value; //input
-  const list = document.getElementById('lista-tarefas'); //ol
   const newValue = document.createElement('li'); //li
   newValue.appendChild(document.createTextNode(inputValue));
   list.appendChild(newValue);
@@ -12,6 +11,12 @@ function addTask() {
 }
 button.addEventListener('click', addTask);
 
+function changeColor(event) {
+  let grayColor = 'rgb(128,128,128)'
+  let clickedLi = event.target
+  clickedLi.style.backgroundColor = grayColor
+}
+list.addEventListener('click', changeColor);
 
 
 

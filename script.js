0e9a946433793b97eed12eee971fdbbe068718a6
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-06-07 00:39:30 -0300

    adiciona ajustes do lint

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let list = document.getElementById('lista-tarefas'); //ol
+const list = document.getElementById('lista-tarefas'); // ol
 
 // 5 e 6
 const button = document.getElementById('criar-tarefa');
 function addTask() {
   //  pegar o conteudo do input e adicionar como uma nova li da ol
-  let inputValue = document.querySelector('input').value; //input
-  const newValue = document.createElement('li'); //li
+  const inputValue = document.querySelector('input').value; // input
+  const newValue = document.createElement('li'); // li
   newValue.appendChild(document.createTextNode(inputValue));
   list.appendChild(newValue);
   document.querySelector('input').value = '';
 }
 button.addEventListener('click', addTask);
 
-
-// 7 
+// 7
 function changeColor(event) {
-  let grayColor = 'rgb(128,128,128)';
-  let clickedLi = event.target;
-  clickedLi.style.backgroundColor = grayColor;     
+  const grayColor = 'rgb(128,128,128)';
+  const clickedLi = event.target;
+  clickedLi.style.backgroundColor = grayColor;
 }
 list.addEventListener('click', changeColor);
 
@@ -30,7 +29,7 @@ list.addEventListener('click', changeColor);
 
 // 8
 function removeSelectedClass() {
-  let li = document.getElementsByTagName('li');
+  const li = document.getElementsByTagName('li');
   for (let index = 0; index < li.length; index += 1) {
     if (li[index].classList.value === 'selected') {
       li[index].className = '';
@@ -40,14 +39,14 @@ function removeSelectedClass() {
 
 function changeSelectedClass(event) {
   removeSelectedClass();
-  let clickedLi = event.target
+  const clickedLi = event.target;
   clickedLi.classList.add('selected');
 }
 list.addEventListener('click', changeSelectedClass);
 
 // 9
 function addCompletedClass(event) {
-  let clickedLi = event.target
+  const clickedLi = event.target;
   clickedLi.classList.add('completed');
 }
 list.addEventListener('dblclick', addCompletedClass);
@@ -55,22 +54,16 @@ list.addEventListener('dblclick', addCompletedClass);
 // 10
 const clearButton = document.getElementById('apaga-tudo');
 function clearAll() {
-  list.innerHTML = ''
+  list.innerHTML = '';
 }
-clearButton.addEventListener('click', clearAll)
+clearButton.addEventListener('click', clearAll);
 
 // 11
 const removeButton = document.getElementById('remover-finalizados');
-function removeFinishedItems(){
+function removeFinishedItems() {
   const finishedItems = document.getElementsByClassName('completed');
-  while(finishedItems.length > 0){
-      finishedItems[0].parentNode.removeChild(finishedItems[0]);
+  while (finishedItems.length > 0) {
+    finishedItems[0].parentNode.removeChild(finishedItems[0]);
   }
 }
 removeButton.addEventListener('click', removeFinishedItems);
-
-
-
-
-
-
